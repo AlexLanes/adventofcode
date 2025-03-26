3721bb1de84dcd966c32589ac2b9c99882808dfe
Alex Angelo <alex_lanes@hotmail.com>
2025-03-26 16:26:09 -0300

    2021 day 12

---
+++ 2021_12.py
@@ -0,0 +1,66 @@
+class PassagePathing:
+
+    paths: dict[str, set[str]]
+
+    def __init__ (self, s: str) -> None:
+        self.paths = {}
+        for line in s.split("\n"):
+            a, b = line.split("-")
+            if a not in self.paths:
+                self.paths[a] = set()
+            self.paths[a].add(b)
+            if b not in self.paths:
+                self.paths[b] = set()
+            self.paths[b].add(a)
+
+    def paths_possible (self) -> int:
+        possible = 0
+        paths = [["start"]]
+
+        while paths:
+            path = paths.pop(-1)
+            last = path[-1]
+            if last == "end":
+                possible += 1
+                continue
+
+            for cave in self.paths[last]:
+                if not (cave not in path or cave.isupper()):
+                    continue
+                paths.append([*path, cave])
+
+        return possible
+
+example = """start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"""
+puzzle = """QF-bw
+end-ne
+po-ju
+QF-lo
+po-start
+XL-ne
+bw-US
+ne-lo
+nu-ne
+bw-po
+QF-ne
+ne-ju
+start-lo
+lo-XL
+QF-ju
+end-ju
+XL-end
+bw-ju
+nu-start
+lo-nu
+nu-XL
+xb-XL
+XL-po"""
+
+assert PassagePathing(example).paths_possible() == 10
+print("Part 1:", PassagePathing(puzzle).paths_possible())
